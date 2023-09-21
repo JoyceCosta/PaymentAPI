e70ddeb25708c6b57a1ad0fe3fc25e355553b8e3
JoyceCosta <joycecosta90@gmail.com>
2023-09-21 20:30:57 +0100

    first commit

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        public string CardOwnerName { get; set; } 
+
+        public string CardNumber { get; set; }
+
+        public string ExpirationDate { get; set; }
+
+        public string SecurityCodes { get; set; }
+    }
+}
