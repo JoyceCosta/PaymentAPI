d6c9060f3a8f61247b5246b8a4b0c7e7904391df
JoyceCosta <joycecosta90@gmail.com>
2024-02-06 19:04:43 +0000

    configure/enable cors policy

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  url:string = environment.apiBaseUrl + 'PaymentDetail'
+  url:string = environment.apiBaseUrl + '/PaymentDetail'
   constructor(private http: HttpClient) { }
 
   refreshList(){
